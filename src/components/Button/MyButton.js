2aa0477d0b7316aa0b739a08bb8a799f9d1290da
Steve <ycsteven.tsao@gmail.com>
2020-05-26 16:48:39 +0800

    prop 型別驗證

--- src/components/Button/MyButton.js
+++ src/components/Button/MyButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
+import PropTypes from "prop-types";
 
 const MyButton = (props) => (
   <TouchableOpacity onPress={props.custOnPress}>
@@ -8,3 +9,12 @@ const MyButton = (props) => (
 );
 
 export default MyButton;
+
+MyButton.propTypes = {
+  custTitle: PropTypes.string.isRequired,
+  custOnPress: PropTypes.func.isRequired,
+};
+
+MyButton.defaultProps = {
+  custTitle: "Hello",
+};
