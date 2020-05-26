a2fe99f410bc240f9b30a313ac4b2c6d1e917f9f
Steve <ycsteven.tsao@gmail.com>
2020-05-26 15:14:58 +0800

    自製按鈕改 arrow function

--- src/components/Button/MyButton.js
+++ src/components/Button/MyButton.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
 
-function MyButton(props) {
-  return (
-    <TouchableOpacity onPress={props.custOnPress}>
-      <Text>{props.custTitle}</Text>
-    </TouchableOpacity>
-  );
-}
+const MyButton = (props) => (
+  <TouchableOpacity onPress={props.custOnPress}>
+    <Text>{props.custTitle}</Text>
+  </TouchableOpacity>
+);
 
 export default MyButton;
