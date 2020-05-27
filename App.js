c4a9f159fc06dfd1f2ce5dd3123dc21b6e4717e2
Steve <ycsteven.tsao@gmail.com>
2020-05-27 11:24:43 +0800

    加上 icon 和 style

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MyButton from "./src/components/Button/MyButton";
+// import MyButton from "./src/components/Button/MyButton";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import HomeScreen from "./src/screens/HomeScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
 
+const Tab = createBottomTabNavigator();
 export default function App() {
   const pressMe = () => {
     console.log("你按到我了！");
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <MyButton custTitle={"按我吧！"} custOnPress={() => pressMe()} />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <MyButton custTitle={"按我吧！"} custOnPress={() => pressMe()} />
+    // </View>
+
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={() => ({
+          tabBarIcon: ({ color }) => (
+            <Ionicons name={"ios-aperture"} size={25} color={color} />
+          ),
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MyButton from "../components/Button/MyButton";
+
+export default function ProfileScreen() {
+  const pressMe = () => {
+    console.log("你按到我了！");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <MyButton custTitle={"按我吧！"} custOnPress={() => pressMe()} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
