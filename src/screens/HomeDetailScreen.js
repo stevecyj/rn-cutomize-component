084d9e9b9e2c6668eccf2be3c0599cc82b151ebf
Steve <ycsteven.tsao@gmail.com>
2020-05-28 01:16:55 +0800

    使用變數，簡化資料傳遞程式碼的長度

--- src/screens/HomeDetailScreen.js
+++ src/screens/HomeDetailScreen.js
@@ -7,11 +7,12 @@ export default function HomeDetailScreen(props) {
     console.log("你按到 HomeDetailScreen 了！");
   };
 
+  const name = props.route.params.name || "nothing get";
   return (
     <View style={styles.container}>
       <Text>我是 HomeDetailScreen 嘻嘻</Text>
       <Button title="回上一頁" onPress={() => props.navigation.pop()} />
-      <Text>{props.route.params.name}</Text>
+      <Text>{name}</Text>
       <MyButton custTitle={"按我吧！"} custOnPress={() => pressMe()} />
     </View>
   );
