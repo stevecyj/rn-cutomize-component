ec74c1a4417116e266d048e2e42059a50d3e6d86
Steve <ycsteven.tsao@gmail.com>
2020-05-28 07:05:35 +0800

    FlatList
    先排好顯示內容標籤的位置

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,77 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import MyButton from "../components/Button/MyButton";
+
+var MOCKED_DATA = [
+  {
+    id: "1",
+    note: "恭喜您！達成環島100次",
+    date: "2020/05/20 14:00",
+  },
+  {
+    id: "2",
+    note: "您的會員身份認證已經審核通過！",
+    date: "2020/05/21 16:00",
+  },
+  {
+    id: "3",
+    note: "撥款通知：本公司已將款項$12345撥入到您指定的銀行帳戶",
+    date: "2020/05/22 09:00",
+  },
+  {
+    id: "4",
+    note: "恭喜您！達成環島100次",
+    date: "2020/05/20 14:00",
+  },
+  {
+    id: "5",
+    note: "您的會員身份認證已經審核通過！",
+    date: "2020/05/21 16:00",
+  },
+  {
+    id: "6",
+    note: "撥款通知：本公司已將款項$12345撥入到您指定的銀行帳戶",
+    date: "2020/05/22 09:00",
+  },
+  {
+    id: "7",
+    note: "恭喜您！達成環島100次",
+    date: "2020/05/20 14:00",
+  },
+  {
+    id: "8",
+    note: "您的會員身份認證已經審核通過！",
+    date: "2020/05/21 16:00",
+  },
+  {
+    id: "9",
+    note: "撥款通知：本公司已將款項$12345撥入到您指定的銀行帳戶",
+    date: "2020/05/22 09:00",
+  },
+  {
+    id: "10",
+    note: "恭喜您！達成環島100次",
+    date: "2020/05/20 14:00",
+  },
+  {
+    id: "11",
+    note: "您的會員身份認證已經審核通過！",
+    date: "2020/05/21 16:00",
+  },
+  {
+    id: "12",
+    note: "撥款通知：本公司已將款項$12345撥入到您指定的銀行帳戶",
+    date: "2020/05/22 09:00",
+  },
+];
 
 export default function HomeScreen(props) {
   const pressMe = () => {
@@ -8,13 +79,38 @@ export default function HomeScreen(props) {
   };
 
   const [food, setFood] = useState("candy");
+  const [dataSource, setDataSource] = useState([]);
 
   const changeFood = (foodGet) => {
     setFood(foodGet);
   };
+
+  useEffect(() => {
+    var book = MOCKED_DATA;
+    setDataSource(book);
+  });
+
+  const renderBook = (cases) => {
+    return (
+      <TouchableOpacity>
+        <View>
+          <View>
+            <Image />
+
+            <View>
+              <Text></Text>
+
+              <Text></Text>
+            </View>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>我是 HomeScreen</Text>
+      {/* <Text>我是 HomeScreen</Text>
       <Text>{food}</Text>
       <Button
         title="到下一頁"
@@ -25,7 +121,13 @@ export default function HomeScreen(props) {
           })
         }
       />
-      <MyButton custTitle={"按我吧！"} custOnPress={() => pressMe()} />
+      <MyButton custTitle={"按我吧！"} custOnPress={() => pressMe()} /> */}
+      <FlatList
+        data={dataSource}
+        renderItem={(cases) => renderBook(cases.item)}
+        keyExtractor={(cases) => cases.id}
+        style={{ backgroundColor: "#fce8d5" }}
+      />
     </View>
   );
 }
